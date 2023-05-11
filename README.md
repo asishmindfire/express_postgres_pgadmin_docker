312903d69d997faa15a9c10e6d9b6c608ef80d1a
asishmindfire <mfsi.asish.s@gmail.com>
2023-05-11 20:25:09 +0530

    first commit

---
+++ README.md
@@ -0,0 +1,5 @@
+# To start see the pgAdmin UI please visit
+
+~ http://localhost:5050
+
+# Then add the database ip, username and password
---
+++ app/index.js
@@ -0,0 +1,31 @@
+const express = require('express');
+
+const sequelize = require('./util/database');
+const User = require('./models/users');
+
+const app = express();
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true}));
+
+app.use((req, res, next) => {
+  res.setHeader('Access-Control-Allow-Origin','*');
+  res.setHeader('Access-Control-Allow-Methods', 'GET','POST','PUT','DELETE');
+  next();
+})
+
+app.use('/dev', require('./routes/dev'));
+app.use('/users', require('./routes/users'));
+
+(async () =>{
+  try {
+    // This will connect to postgres server and create all tables
+    await sequelize.sync(
+      {force: false}
+    );
+    console.log("test");
+    app.listen(process.env.EXTERNAL_PORT || 5000);
+  } catch (error) {
+    console.error(error);
+  }
+})()
